82c3f9e19a480160347a152a86a18aad6b3dc150
jahnellybean <jmadarang87@gmail.com>
2020-11-29 23:43:53 -0600

    added birthday countdown

--- app.js
+++ app.js
@@ -39,8 +39,35 @@ Sets Age
 const myAge = document.getElementById('age');
 
 const birthday = new Date(1987, 12, 3);
-const today = new Date();
+let today = new Date();
 let age = ( today - birthday ) / 1000 / 60 / 60 / 24 / 365.25;
 age = Math.floor(age);
-
 myAge.textContent = `Age: ${age}`;
+
+
+/* -------------------------- 
+Sets Countdown to Birthday!
+-------------------------- */ 
+
+const countdownText = document.getElementById('countdown');
+
+const birthdayCountdown = function () {
+    let nextBirthday = new Date("Dec 3, 2020").getTime();
+    const today = new Date();
+    let countdown = nextBirthday - today;
+    countdown = new Date(countdown);
+    let days = Math.floor(countdown / 1000 / 60 / 60 /24);
+    let hours = countdown.getHours();
+    let minutes = countdown.getMinutes();
+    let seconds = countdown.getSeconds();
+
+    // const countdown = `${days} days til my birthday! ${hours} hours til my birthday! 
+    //     ${minutes} minutes til my birthday! ${seconds} seconds til my birthday!`
+
+    const message = `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds til my birthday!! `;
+
+    countdownText.textContent = `${message}`;
+
+};
+
+setInterval(birthdayCountdown, 1000);
